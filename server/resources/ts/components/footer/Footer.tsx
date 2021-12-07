036f7d4a7dd24e9d32ec4b3e6348af89d25aae5f
io-kobayashiii <io.kobayashiii@gmail.com>
2021-12-07 21:45:35 +0900

    #24 - add layout

--- server/resources/ts/components/footer/Footer.tsx
+++ server/resources/ts/components/footer/Footer.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 
 const Footer = (): JSX.Element => {
 	return (
-		<footer className={`border-t border-gray-200 h-56`}>
-			<div className={`h-100p flex justify-center items-center`}>
-				<p className={`text-12`}>&copy; EISHODO All Rights Reserved.</p>
+		<footer className={`border-t border-gray-200`}>
+			<div className={`h-56 flex justify-center items-center`}>
+				<p className={`text-12`}>&copy; CompanyName All Rights Reserved.</p>
 			</div>
 		</footer>
 	)
