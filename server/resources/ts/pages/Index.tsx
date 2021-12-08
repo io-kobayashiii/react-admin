4cb4ee892f311562095f9b217fa22342cae17a54
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-12-08 21:57:00 +0900

    close #57

--- server/resources/ts/pages/Index.tsx
+++ server/resources/ts/pages/Index.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react'
 import Default from '../layouts/DefaultLayout'
+import axios from 'axios'
 
 const Index = (): JSX.Element => {
+	const env = process.env.MIX_FRONT_APP_ENV
+	const baseURL = env === 'mock' ? 'http://localhost:8084' : env === 'development' ? 'http://api.localhost' : env === 'staging' ? 'https://test-api.example.com' : 'https://api.example.com '
+
+	let axiosBase = axios.create({
+		baseURL: baseURL,
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Requested-With': 'XMLHttpRequest'
+		},
+		responseType: 'json'
+	})
+
+	axiosBase.get('/companies').then(res => {
+		console.log(res.data)
+	})
+
+
 	const breadcrumbs = [
 		{
 			displayText: 'HOME',
@@ -13,9 +31,9 @@ const Index = (): JSX.Element => {
 		},
 	]
 	return (
-		<Default breadcrumbs={breadcrumbs}>
+		<Default breadcrumbs={breadcrumbs} >
 			<div className={`bg-gray-600`}></div>
-		</Default>
+		</Default >
 	)
 }
 
