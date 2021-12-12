27e2cfeedc0185df7d55eb9918c949c561523e61
io-kobayashiii <io.kobayashiii@gmail.com>
2021-12-12 17:43:30 +0900

    add badge property to Label component #55

--- server/resources/ts/components/form/Label.tsx
+++ server/resources/ts/components/form/Label.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react'
+import Badge from '../common/Badge'
 
 type _Props = {
 	displayText: string
+	badge?: 'required' | 'optional'
 	htmlFor?: string
 	className?: string
 }
 
-const Label = ({ displayText, htmlFor, className }: _Props): JSX.Element => {
+const Label = ({ displayText, badge, htmlFor, className }: _Props): JSX.Element => {
 	return (
-		<label htmlFor={htmlFor ? htmlFor : htmlFor} className={`${className ? className : ''} pt-10 pr-16`}>
+		<label htmlFor={htmlFor ? htmlFor : htmlFor} className={`${className ? className : ''} pt-10`}>
 			{displayText}
+			{badge && (
+				<Badge
+					size={'medium'}
+					color={badge && badge == 'required' ? 'red' : 'green'}
+					displayText={badge && badge == 'required' ? '必須' : badge == 'optional' ? '任意' : displayText}
+					className={'ml-8 h-fit'}
+				/>
+			)}
 		</label>
 	)
 }
